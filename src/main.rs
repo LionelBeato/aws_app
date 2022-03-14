72506abc4b258c8895722184c2e7e4797b0394c7
Lionel Beato <lionel.beato@gmail.com>
2022-03-14 16:38:48 -0400

    add comments

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use aws_sdk_dynamodb::{Client, Error};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    // defines our region
     let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
+    // defines our config from env variables
     let config = aws_config::from_env().region(region_provider).load().await;
-
+    // new client instance
     let client = Client::new(&config); // passing config by reference
-
+    // variable representing dynamodb result
     let resp = client.list_tables().send().await?;
     
     println!("Tables:"); 
@@ -20,5 +22,6 @@ async fn main() -> Result<(), Error> {
 
     println!("Found {} tables", names.len()); 
 
+    // return value of type Result<>
     Ok(())
 }
